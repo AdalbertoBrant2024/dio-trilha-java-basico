a9e362aea63159b2f885208c76240c2a7140fefa
adalbertobrant <adalbertobrant@gmail.com>
2024-04-27 16:27:27 -0300

    commit inicial do projeto

---
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -0,0 +1,23 @@
+
+package contabanco;
+
+/**
+ *
+ * @author adalberto
+ * @version 0.0.1
+ */
+public class ContaTerminal {
+
+    /**
+     * @param args[0] 
+     * @param args[1] numero da conta corrente
+     * @param args[2] texto da agencia
+     * @param args[3] nome do cliente
+     * @param args[4] saldo
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
