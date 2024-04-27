d0866d31863fb8d21bcc2e392f884c0dbfa78a60
adalbertobrant <adalbertobrant@gmail.com>
2024-04-27 17:15:14 -0300

    trabalho final do projeto

--- ContaBanco/src/contabanco/ContaTerminal.java
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -1,23 +1,96 @@
 
 package contabanco;
 
+import java.util.Scanner;
+
 /**
  *
- * @author adalberto
+ * @author adalberto caldeira
  * @version 0.0.1
  */
 public class ContaTerminal {
 
     /**
-     * @param args[0] 
-     * @param args[1] numero da conta corrente
-     * @param args[2] texto da agencia
-     * @param args[3] nome do cliente
-     * @param args[4] saldo
+     * @param args argumentos não será usado
+     * @param numero é inteiro e representa o número da conta
+     * @param agencia é uma string do tipo xxx-xx
+     * @param nomeCliente string com o nome do cliente
+     * @param saldo é um Decimal com o saldo em conta do cliente
      */
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+    
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        String msg = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.";
+        return msg;
+        
+    }
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        ContaTerminal contaTerminal = new ContaTerminal();
+        
+        // TODO: Aprender classe scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+        contaTerminal.setAgencia(agencia);
+        
+        System.out.println("Por favor, digite o número da conta !");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        contaTerminal.setNumero(numero);
+        
+        System.out.println("Por favor, entre o nome do cliente !");
+        String nome = scanner.nextLine();
+        contaTerminal.setNomeCliente(nome);
+        
+        System.out.println("Por favor, entre o saldo do cliente !");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+        contaTerminal.setSaldo(saldo);
+        
+        System.out.println("  ");
+        System.out.println("  ");
+        System.out.println("  ");
+        System.out.println(contaTerminal.toString());
     }
     
 }
