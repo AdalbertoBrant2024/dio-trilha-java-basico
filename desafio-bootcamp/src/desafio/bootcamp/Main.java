618b99b444e336d821385683c2f28d8e9a23920b
adalbertobrant <adalbertobrant@gmail.com>
2024-07-13 06:03:40 -0300

    teste das classes feito com sucesso

--- desafio-bootcamp/src/desafio/bootcamp/Main.java
+++ desafio-bootcamp/src/desafio/bootcamp/Main.java
@@ -1,13 +1,30 @@
 
 package desafio.bootcamp;
+// importando os pacotes e classes criados
 import desafio.bootcamp.dominio.cursos.Curso;
 import desafio.bootcamp.dominio.mentorias.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
         
+        curso.setTitulo("Curso de Java");
+        mentoria.setTitulo("Mentoria Básica de Java");
+        curso.setDescricao("Curso de fundamentos de POO");
+        mentoria.setDescricao("Fazendo um bootcamp em POO");
+        curso.setCargaHoraria(2);
+        mentoria.setData(LocalDate.now());
+        
+        // Imprimindo para teste da toString()
+        
+        System.out.println(curso.toString());
+        System.out.println("--------------------");
+        System.out.println(mentoria.toString());
     }
     
 }
