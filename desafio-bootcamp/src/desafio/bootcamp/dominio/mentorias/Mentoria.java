5093fce2a9eee5fd22904f645584d3753d61626b
adalbertobrant <adalbertobrant@gmail.com>
2024-07-13 06:33:35 -0300

    refatoração em andamento

--- desafio-bootcamp/src/desafio/bootcamp/dominio/mentorias/Mentoria.java
+++ desafio-bootcamp/src/desafio/bootcamp/dominio/mentorias/Mentoria.java
@@ -1,28 +1,13 @@
 
 package desafio.bootcamp.dominio.mentorias;
 
+import desafio.bootcamp.dominio.conteudos.Conteudo;
+
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+    
     private LocalDate data;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -35,9 +20,14 @@ public class Mentoria {
     @Override
     public String toString() {
         return "*** Mentoria *** " 
-                + "\ntitulo = " + titulo 
-                + "\ndescricao = " + descricao + 
+                + "\ntitulo = " + super.getTitulo() 
+                + "\ndescricao = " + super.getDescricao() + 
                 "\ndata = " + data + "\n";
+    }
+
+    @Override
+    public double calcularXp() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
     
