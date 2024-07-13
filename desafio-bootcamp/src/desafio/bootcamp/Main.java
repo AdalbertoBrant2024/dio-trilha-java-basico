53c31e0ad653145e41cf4eb65e5b553a4b6b0658
adalbertobrant <adalbertobrant@gmail.com>
2024-07-13 07:53:34 -0300

    finalizado

--- desafio-bootcamp/src/desafio/bootcamp/Main.java
+++ desafio-bootcamp/src/desafio/bootcamp/Main.java
@@ -1,6 +1,8 @@
 
 package desafio.bootcamp;
 // importando os pacotes e classes criados
+import desafio.bootcamp.bootcamp.Bootcamp;
+import desafio.bootcamp.dev.Dev;
 import desafio.bootcamp.dominio.cursos.Curso;
 import desafio.bootcamp.dominio.mentorias.Mentoria;
 
@@ -20,15 +22,39 @@ public class Main {
         curso.setCargaHoraria(2);
         mentoria.setData(LocalDate.now());
         
-        // Imprimindo para teste da toString()
+        // Criação do BootCamp e adição do curso e Mentoria
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp Java");
+        bootcamp.setDescricao("Aprendendo mais sobre Java com muita prática");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
         
-        System.out.println(curso.toString());
-        System.out.println("--------------------");
-        System.out.println(mentoria.toString());
-        System.out.println("--------------------");
-        System.out.println("Xp do Curso -> "+curso.calcularXp());
-        System.out.println("--------------------");
-        System.out.println("Xp da Mentoria -> "+ mentoria.calcularXp());
+        // Criação do Dev
+        Dev devJose = new Dev();
+        devJose.setNome("José da Silva");
+        devJose.inscreverBootcamp(bootcamp);
+        
+        Dev devCamila = new Dev();
+        devCamila.setNome("Cami-la");
+        devCamila.inscreverBootcamp(bootcamp);
+        
+        // Ver os conteúdos Inscritos
+        System.out.println("Dev José - Conteúdos inscritos-> "+devJose.getConteudosInscritos());
+        System.out.println("Dev Camila - Conteúdos inscritos-> "+devCamila.getConteudosInscritos());
+        
+        // Ver os conteúdos Concluídos
+        System.out.println("Dev José\nConteúdos Concluídos\n"+ devJose.getConteudosConcluidos());
+        System.out.println("Dev Camila\nConteúdos Concluídos\n"+ devCamila.getConteudosConcluidos());
+        
+        // José conclui todos os cursos e mentoria Camila apenas Mentoria
+        devJose.progredir();
+        devJose.progredir();
+        devCamila.progredir();
+        if(devJose.getConteudosInscritos().isEmpty()){
+            System.out.println("Parabéns José\n BootCamp Concluído\n"+devJose.getConteudosConcluidos());
+            System.out.println("XP total -> "+devJose.calcularTotalXp());
+        }
+        System.out.println("Dev Camila\nConteúdos Concluídos\n"+ devCamila.getConteudosConcluidos());
+        System.out.println("XP Parcial -> "+devCamila.calcularTotalXp());
     }
-    
 }
